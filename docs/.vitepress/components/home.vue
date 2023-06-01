cd004c0d748c124ea5b3093c7295b86dfc3a0e56
Eagon Ellington <gyhoshi@gmail.com>
2023-06-01 14:54:48 +0800

    -fix:homeStyle

--- docs/.vitepress/components/home.vue
+++ docs/.vitepress/components/home.vue
@@ -15,6 +15,7 @@ const svg = `
     />
   </svg>
 `;
+const vueSvg = `<svg t="1685599771613" class="icons" viewBox="0 -100 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2405" width="80" height="60"><path d="M512 504L764.8 68.8h-134.4L512 275.2 393.6 70.4h-134.4L512 504zM819.2 70.4L512 600 204.8 68.8H0l512 886.4L1024 70.4H819.2z" fill="#13227a" p-id="2406"></path></svg>`;
 </script>
 <template>
   <div>
@@ -36,7 +37,7 @@ const svg = `
       </div>
       <div class="drop" style="--clr: #01b4ff">
         <div class="content">
-          <h2 v-html="svg"></h2>
+          <h2 v-html="vueSvg"></h2>
           <p style="font-weight: 800">VUE</p>
           <a href="#"></a>
         </div>
